48885e990efff0835d13f4db025cb4315e6e9b81
zaliberty <dream_vital@mail.ru>
2019-03-31 15:54:46 +0300

    Добавлена строка, выводящая ссобщение "Эту строку добавил Виталик"

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,6 +13,8 @@ namespace ConsoleApp1
             Console.WriteLine("Privet");
             Console.WriteLine("Poka");
 
+            Console.WriteLine("Эту строку добавил Виталик");
+
             Console.ReadKey();
 
 
